ae87c32f32d2da1b616bf2e23faf20c486d584ba
YUNNONG <tigermon@naver.com>
2022-02-11 20:32:49 +0900

    2022-02-11 : 2월 10일 합친 것

--- Project_BongPirate/Assets/02.Scripts/CombatSystem/CombatManager.cs
+++ Project_BongPirate/Assets/02.Scripts/CombatSystem/CombatManager.cs
@@ -24,6 +24,28 @@ public class CombatManager : MonoBehaviour
         {
             EquipCannon(2, 0);
         }
+        else if (Input.GetKeyDown(KeyCode.Alpha4))
+        {
+            EquipCannon(3, 0);
+        }
+
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            EquipSail(0, 0);
+        }
+        else if (Input.GetKeyDown(KeyCode.O))
+        {
+            EquipSail(0, 1);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            EquipSpecialCannon(0, 0);
+        }
+        else if (Input.GetKeyDown(KeyCode.X))
+        {
+            EquipSpecialCannon(1, 0);
+        }
     }
 
     public void SetMyShip(Player_Combat_Ship _ship)
@@ -31,13 +53,26 @@ public class CombatManager : MonoBehaviour
         myShip = _ship;
     }
 
+    private void EquipSail(int _spotIndex, int _sailIndex)
+    {
+        myShip.GetComponent<Photon.Pun.PhotonView>().RPC("EquipSail", Photon.Pun.RpcTarget.AllBuffered, new object[] { _spotIndex, _sailIndex });
+    }
     private void EquipCannon(int _spotIndex, int _cannonIndex)
     {
         //Cannon tmpCannon = myShip.EquipCannon(_spotIndex, _cannonIndex);
         myShip.GetComponent<Photon.Pun.PhotonView>().RPC("EquipCannon", Photon.Pun.RpcTarget.AllBuffered, new object[] { _spotIndex, _cannonIndex });
     }
-    public void AddCannonType(int param)
+    private void EquipSpecialCannon(int _spotIndex, int _cannonIndex)
     {
-        myShip.ChangeCannonType(param, 1,false);
+        //Cannon tmpCannon = myShip.EquipCannon(_spotIndex, _cannonIndex);
+        myShip.GetComponent<Photon.Pun.PhotonView>().RPC("EquipSpecialCannon", Photon.Pun.RpcTarget.AllBuffered, new object[] { _spotIndex, _cannonIndex });
+    }
+    public void AddAutoCannonType(int param)
+    {
+        myShip.ChangeCannonType(param, 1, false);
+    }
+    public void AddSpecialCannonType(int param)
+    {
+        myShip.ChangeSpecialCannonType(param, 1, false);
     }
 }
--- Project_BongPirate/Assets/02.Scripts/CombatSystem/Player_Combat_Ship.cs
+++ Project_BongPirate/Assets/02.Scripts/CombatSystem/Player_Combat_Ship.cs
@@ -6,18 +6,107 @@ public class Player_Combat_Ship : MonoBehaviourPun
     [SerializeField] private float health;
     [SerializeField] private float maxHealth;
 
-    [SerializeField] private List<Transform> CannonSpots;
+    [SerializeField] private List<GameObject> shipObjects;
+    private GameObject myShipObjects;
 
-    [SerializeField] private List<Cannon> myCannons;
+    // level 1: 1, level 2: 1, level 3: 2, level4 : 3 
+    [SerializeField] private List<Transform> SailSpots;
+
+    // level 1: 0, level 2: 1, level 3: 1, level4 : 2 
+    [SerializeField] private List<Transform> SpecialCannonSpots;
+
+    // level 1: 1, level 2: 2, level 3: 4, level4 : 4 
+    [SerializeField] private List<Transform> AutoCannonSpots;
+
+    [SerializeField] private List<Cannon> mySpecialCannons;
+    [SerializeField] private List<Cannon> myAutoCannons;
+    [SerializeField] private List<GameObject> mySails;
 
 
     private void Start()
     {
         health = maxHealth;
-        for (int i=0;i< CannonSpots.Count; i++)
+        GetComponent<Photon.Pun.PhotonView>().RPC("InitializeCombat", Photon.Pun.RpcTarget.AllBuffered, 0);
+    }
+    [PunRPC]
+    public void InitializeCombat(int param)
+    {
+        for (int i = 0; i < shipObjects.Count; i++)
         {
-            myCannons.Add(null);
+            shipObjects[i].gameObject.SetActive(false);
         }
+        myShipObjects = shipObjects[(int)param];
+        myShipObjects.SetActive(true);
+
+        maxHealth += (int)param * 50f;
+        health += (int)param * 50f;
+
+        AutoCannonSpots.Clear();
+        SpecialCannonSpots.Clear();
+        SailSpots.Clear();
+
+        for (int i = 0; i < myShipObjects.transform.Find("CannonSpots").childCount; i++)
+            AutoCannonSpots.Add(myShipObjects.transform.Find("CannonSpots").GetChild(i));
+        for (int i = 0; i < myShipObjects.transform.Find("SpecialCannonSpots").childCount; i++)
+            SpecialCannonSpots.Add(myShipObjects.transform.Find("SpecialCannonSpots").GetChild(i));
+        for (int i = 0; i < myShipObjects.transform.Find("SailSpots").childCount; i++)
+            SailSpots.Add(myShipObjects.transform.Find("SailSpots").GetChild(i));
+
+
+        for (int i = 0; i < myAutoCannons.Count; i++)
+        {
+            if (myAutoCannons[i] != null)
+            {
+                myAutoCannons[i].transform.SetParent(AutoCannonSpots[i]);
+                myAutoCannons[i].transform.localPosition = Vector3.zero;
+                myAutoCannons[i].transform.localRotation = Quaternion.identity;
+            }
+        }
+        for (int i = 0; i < mySpecialCannons.Count; i++)
+        {
+            if (mySpecialCannons[i] != null)
+            {
+                mySpecialCannons[i].transform.SetParent(SpecialCannonSpots[i]);
+                mySpecialCannons[i].transform.localPosition = Vector3.zero;
+                mySpecialCannons[i].transform.localRotation = Quaternion.identity;
+            }
+        }
+        for (int i = 0; i < mySails.Count; i++)
+        {
+            if (mySails[i] != null)
+            {
+                mySails[i].transform.SetParent(SailSpots[i]);
+                mySails[i].transform.localPosition = Vector3.zero;
+                mySails[i].transform.localRotation = Quaternion.identity;
+            }
+        }
+
+        for (int i = 0; i < AutoCannonSpots.Count; i++)
+        {
+            if (myAutoCannons.Count < AutoCannonSpots.Count)
+                myAutoCannons.Add(null);
+        }
+        for (int i = 0; i < SpecialCannonSpots.Count; i++)
+        {
+            if (mySpecialCannons.Count < SpecialCannonSpots.Count)
+                mySpecialCannons.Add(null);
+        }
+        for (int i = 0; i < SailSpots.Count; i++)
+        {
+            if (mySails.Count < SailSpots.Count)
+                mySails.Add(null);
+        }
+    }
+
+    public float GetSailSpeed()
+    {
+        float tmp = 0;
+        for (int i = 0; i < mySails.Count; i++)
+        {
+            if (mySails[i] != null)
+                tmp += 5f;
+        }
+        return tmp;
     }
 
     [PunRPC]
@@ -36,6 +125,35 @@ public class Player_Combat_Ship : MonoBehaviourPun
             {
                 go.GetComponent<TreasureChest>().items.Add(Item_Manager.instance.Player_items[i]);
             }
+            if (photonView.IsMine)
+            {
+                UnityEngine.SceneManagement.SceneManager.LoadScene(0, UnityEngine.SceneManagement.LoadSceneMode.Single);
+            }
+        }
+    }
+
+    [PunRPC]
+    public void EquipSail(int _spotIndex, int _sailIndex)
+    {
+        if (mySails[_spotIndex] == null)
+        {
+            GetComponent<Player_Controller_Ship>().MaxSpeed += 5f;
+            GetComponent<Player_Controller_Ship>().MoveSpeed += 5f;
+
+            GameObject tmpCannon = null;
+            tmpCannon = Instantiate(Resources.Load("Sail_" + _sailIndex) as GameObject, Vector3.zero, Quaternion.identity);
+
+            tmpCannon.transform.SetParent(SailSpots[_spotIndex]);
+            tmpCannon.transform.localPosition = Vector3.zero;
+            tmpCannon.transform.localScale = Vector3.one;
+            tmpCannon.transform.localRotation = Quaternion.identity;
+            mySails[_spotIndex] = tmpCannon;
+        }
+        else
+        {
+            GetComponent<Player_Controller_Ship>().MaxSpeed -= 5f;
+            GetComponent<Player_Controller_Ship>().MoveSpeed -= 5f;
+            Destroy(mySails[_spotIndex].gameObject);
         }
     }
 
@@ -43,55 +161,77 @@ public class Player_Combat_Ship : MonoBehaviourPun
     public void EquipCannon(int _spotIndex, int _cannonIndex)
     {
         bool _active = true;
-        if (myCannons[_spotIndex] == null)
+        if (myAutoCannons[_spotIndex] == null)
         {
             GameObject tmpCannon = null;
-            if (_spotIndex == 0)
-                tmpCannon= Instantiate(Resources.Load("Cannon_" + _cannonIndex) as GameObject, Vector3.zero, Quaternion.identity);
-            else
-                tmpCannon = Instantiate(Resources.Load("AutoCannon_" + _cannonIndex) as GameObject, Vector3.zero, Quaternion.identity);
-            tmpCannon.transform.SetParent(CannonSpots[_spotIndex]);
+            tmpCannon = Instantiate(Resources.Load("AutoCannon_" + _cannonIndex) as GameObject, Vector3.zero, Quaternion.identity);
+
+            tmpCannon.transform.SetParent(AutoCannonSpots[_spotIndex]);
             tmpCannon.transform.localPosition = Vector3.zero;
             tmpCannon.transform.localScale = Vector3.one;
             tmpCannon.transform.localRotation = Quaternion.identity;
-            myCannons[_spotIndex] = tmpCannon.GetComponent<Cannon>();
+            myAutoCannons[_spotIndex] = tmpCannon.GetComponent<Cannon>();
             tmpCannon.GetComponent<Cannon>().Initialize(this);
         }
         else
         {
-            print("Destroy");
             _active = false;
-            myCannons[_spotIndex].UnEquipCannon();
+            myAutoCannons[_spotIndex].UnEquipCannon();
         }
 
         if (photonView.IsMine)
         {
             CombatManager combatManager = FindObjectOfType<CombatManager>();
-            if (_spotIndex <= 0)
+            print("joystick : " + _active);
+            combatManager.joySticks[_spotIndex].gameObject.SetActive(_active);
+            if (myAutoCannons[_spotIndex] != null)
             {
-                print("special : "+ _active);
-                combatManager.SpecialJoySticks[_spotIndex].gameObject.SetActive(_active);
-                if (myCannons[_spotIndex] != null)
-                {
-                    myCannons[_spotIndex].tmpJoyStick = combatManager.SpecialJoySticks[_spotIndex];
-                }
+                myAutoCannons[_spotIndex].tmpJoyStick = combatManager.joySticks[_spotIndex];
             }
-            else
+        }
+    }
+    [PunRPC]
+    public void EquipSpecialCannon(int _spotIndex, int _cannonIndex)
+    {
+        bool _active = true;
+        if (mySpecialCannons[_spotIndex] == null)
+        {
+            GameObject tmpCannon = null;
+            tmpCannon = Instantiate(Resources.Load("SpecialCannon_" + _cannonIndex) as GameObject, Vector3.zero, Quaternion.identity);
+            tmpCannon.transform.SetParent(SpecialCannonSpots[_spotIndex]);
+            tmpCannon.transform.localPosition = Vector3.zero;
+            tmpCannon.transform.localScale = Vector3.one;
+            tmpCannon.transform.localRotation = Quaternion.identity;
+            mySpecialCannons[_spotIndex] = tmpCannon.GetComponent<Cannon>();
+            tmpCannon.GetComponent<Cannon>().Initialize(this);
+        }
+        else
+        {
+            _active = false;
+            mySpecialCannons[_spotIndex].UnEquipCannon();
+        }
+
+        if (photonView.IsMine)
+        {
+            CombatManager combatManager = FindObjectOfType<CombatManager>();
+            print("special : " + _active);
+            combatManager.SpecialJoySticks[_spotIndex].gameObject.SetActive(_active);
+            if (mySpecialCannons[_spotIndex] != null)
             {
-                print("joystick : " + _active);
-                combatManager.joySticks[_spotIndex].gameObject.SetActive(_active);
-                if (myCannons[_spotIndex] != null)
-                {
-                    myCannons[_spotIndex].tmpJoyStick = combatManager.joySticks[_spotIndex];
-                }
+                mySpecialCannons[_spotIndex].tmpJoyStick = combatManager.SpecialJoySticks[_spotIndex];
             }
         }
     }
 
-    public void ChangeCannonType(int _spotIndex,int _typeIndex, bool _isSet=true)
+    public void ChangeCannonType(int _spotIndex, int _typeIndex, bool _isSet = true)
     {
-        myCannons[_spotIndex].ChangeCannonType(_typeIndex, _isSet);
+        myAutoCannons[_spotIndex].ChangeCannonType(_typeIndex, _isSet);
     }
+    public void ChangeSpecialCannonType(int _spotIndex, int _typeIndex, bool _isSet = true)
+    {
+        mySpecialCannons[_spotIndex].ChangeCannonType(_typeIndex, _isSet);
+    }
+
 
     private void OnCollisionEnter(Collision collision)
     {
